0ad79a46cd12468b5557529e5957fd8ad98dde39
izaakdale <64863677+izaakdale@users.noreply.github.com>
2023-02-13 23:17:51 +0000

    created list event grpc route

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	db "github.com/izaakdale/service-event/datastore/sqlc"
 	"github.com/izaakdale/service-event/schema/event"
@@ -37,8 +38,25 @@ func (g *GServer) GetEvent(ctx context.Context, ev *event.EventRequest) (*event.
 		EventId:          dbe.EventID,
 		EventName:        dbe.EventName,
 		TicketsRemaining: dbe.TicketsRemaining,
-		Timestamp:        dbe.EventTimestamp.GoString(),
+		Timestamp:        dbe.EventTimestamp.Format(time.RFC3339),
 	}, nil
+}
+
+func (g *GServer) GetEvents(ctx context.Context, le *event.ListEventRequest) (*event.ListEventResponse, error) {
+	dbes, err := querier.GetEvents(ctx, le.EventIds)
+	if err != nil {
+		return nil, err
+	}
+	ret := &event.ListEventResponse{}
+	for _, e := range dbes {
+		ret.Events = append(ret.Events, &event.EventResponse{
+			EventId:          e.EventID,
+			EventName:        e.EventName,
+			TicketsRemaining: e.TicketsRemaining,
+			Timestamp:        e.EventTimestamp.Format(time.RFC3339),
+		})
+	}
+	return ret, nil
 }
 
 func main() {
