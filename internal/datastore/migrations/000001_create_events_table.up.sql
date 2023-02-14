78d411cb9c1421e3947fbc8272423a4ea7ffe61a
izaakdale <64863677+izaakdale@users.noreply.github.com>
2023-02-14 23:54:16 +0000

    added negative tickets constraint

--- internal/datastore/migrations/000001_create_events_table.up.sql
+++ internal/datastore/migrations/000001_create_events_table.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE "events" (
   "event_id" bigserial PRIMARY KEY,
   "event_name" varchar NOT NULL,
   "tickets_remaining" int NOT NULL,
-  "event_timestamp" timestamp NOT NULL
+  "event_timestamp" timestamp NOT NULL,
+  CONSTRAINT "n_of_tickets_valid" CHECK (tickets_remaining >= 0)
 );
 
 CREATE INDEX ON "events" ("event_name");
